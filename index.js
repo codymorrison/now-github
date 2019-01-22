0fe945e0854eeea974b5e355b29eb81d4e5e8bb6
Cody Morrison <cody@pixel43.com>
2019-01-21 16:58:41 -0800

    Move logic to GithubAuth Class
    We can extend this down the road if needed to extend an oAuth class.

--- index.js
+++ index.js
@@ -1,72 +1,29 @@
-require('dotenv').config()
-const querystring = require('querystring')
-const axios = require('axios')
-const { router, get } = require('microrouter');
-const redirect = require('micro-redirect');
-const uid = require('uid-promise');
+const { router, get } = require("microrouter");
 
-const githubUrl = process.env.GH_HOST || 'github.com'
+// Get Github Routes
+const GithubAuth = require("./lib/GithubAuth");
 
-const states = [];
+/**
+ * Setup GH_CLIENT_ID and GH_CLIENT_SECRET.
+ * For security only allow setting by environment variables.
+ */
+const githubClientId = process.env.GH_CLIENT_ID;
+const githubClientSecret = process.env.GH_CLIENT_SECRET;
 
-const redirectWithQueryString = (res, data) => {
-  const location = `${process.env.REDIRECT_URL}?${querystring.stringify(data)}`
-  redirect(res, 302, location)
-}
+/**
+ * Setup GH_HOST. Allow setting the domain for enterprise IDs.
+ * For security only allow setting by environment variables.
+ * If not set, default to github.com.
+ */
+const githubHost = process.env.GH_HOST;
 
-const login = async (req, res) => {
-  const state = await uid(20);
-  states.push(state);
-  const { scope, allow_signup } = req.query;
-  const query = {
-    client_id: process.env.GH_CLIENT_ID,
-    state: state
-  };
-  if (scope) query.scope = scope;
-  if (allow_signup !== undefined) query.allow_signup = allow_signup;
-  redirect(res, 302, `https://${githubUrl}/login/oauth/authorize?${querystring.stringify(query)}`);
-};
+// Initialize Github Auth with defaults
+const app = new GithubAuth(githubClientId, githubClientSecret, {
+  githubUrl: githubHost,
+});
 
-const callback = async (req, res) => {
-
-  res.setHeader('Content-Type', 'text/html')
-  const { code, state } = req.query
-
-  if (!code && !state) {
-    redirectWithQueryString(res, { error: 'Provide code and state query param' })
-  } else if (!states.includes(state)) {
-    redirectWithQueryString(res, { error: 'Unknown state' })
-  } else {
-    states.splice(states.indexOf(state), 1);
-    try {
-      const { status, data } = await axios({
-        method: 'POST',
-        url: `https://${githubUrl}/login/oauth/access_token`,
-        responseType: 'json',
-        data: {
-          client_id: process.env.GH_CLIENT_ID,
-          client_secret: process.env.GH_CLIENT_SECRET,
-          code
-        }
-      })
-
-      if (status === 200) {
-        const qs = querystring.parse(data)
-        if (qs.error) {
-          redirectWithQueryString(res, { error: qs.error_description })
-        } else {
-          redirectWithQueryString(res, { access_token: qs.access_token })
-        }
-      } else {
-        redirectWithQueryString(res, { error: 'GitHub server error.' })
-      }
-    } catch (err) {
-      redirectWithQueryString(res, { error: 'Please provide GH_CLIENT_ID and GH_CLIENT_SECRET as environment variables. (or GitHub might be down)' })
-    }
-  }
-}
-
+// Export routes
 module.exports = router(
-  get('/login', login),
-  get('/callback', callback)
+  get("/login", app.login),
+  get("/callback", app.callback)
 );
